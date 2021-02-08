0d280345ac3a88c7a363a2700f343b614f8f38d2
Jacob Rodriguez <jacobrod@mit.edu>
2021-02-08 09:15:43 -0800

    Edited subscriber to take in range and intensities. Also cleaned up the fake scan publisher code.

--- scripts/fake_scan_publisher.py
+++ scripts/fake_scan_publisher.py
@@ -1,37 +1,4 @@
 #!/usr/bin/env python
-#
-# import rospy
-# import math
-# import numpy as np
-# from sensor_msgs.msg import LaserScan
-#
-# rospy.init_node('fake_scan_publisher', anonymous=True)
-#
-# pub = rospy.Publisher('fake_scan', LaserScan, queue_size=10)
-#
-# rate = rospy.Rate(20)
-#
-#
-# while not rospy.is_shutdown():
-#     scan = LaserScan()
-#
-#     scan.header.frame_id = 'laser_frame'
-#     scan.angle_min = (-2/3 * math.pi)
-#     scan.angle_max = 2/3 * math.pi
-#     scan.angle_increment = (1/300 * math.pi)
-#     scan.range_min = 1
-#     scan.range_max = 10
-#
-#     scan.ranges = []
-#     scan.intensities = []
-#
-#     for x in range(400):
-#         number = np.random.uniform((-2/3 * math.pi, 2/3 * math.pi))
-#         scan.ranges.append(number)
-#
-#     pub.publish(scan)
-#     rate.sleep()
-
 import rospy
 from sensor_msgs.msg import LaserScan
 
--- scripts/open_space_publisher.py
+++ scripts/open_space_publisher.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 
 import rospy
-import math
 import numpy as np
-import sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan
 
 def callback(data):
 
-    rospy.loginfo("", data.something)
+    scan_data = [data.ranges, data.intensities]
+
+    rospy.loginfo(scan_data)
+
+
 
 def listener():
 
-    rospy.init_node("")
+    rospy.init_node("open_space_publisher")
+
+    rospy.Subscriber("master_scan", LaserScan, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
+
+
+
